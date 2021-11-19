bd83fb3c2c6d10536ed77f0af9d17875598bc816
V1a4eslav <dphusevvia4eslav@gmail.com>
2021-11-19 10:42:53 +0200

    Fianal

--- src/app.js
+++ src/app.js
@@ -1,20 +1,37 @@
-swich.addEventListener("click", () => {
-  let theme = document.querySelector(".style");
-  if (theme.getAttribute("href") == "style.css") {
+let theme = document.querySelector(".style");
+let checked = JSON.parse(localStorage.getItem("swap"));
+if (localStorage.getItem("swap")) {
+  ch = JSON.parse(localStorage.getItem("swap"));
+  if (ch == true) {
+    theme.href = "newStyle.css";
+  }
+}
+
+swich.addEventListener("change", () => {
+  swap();
+  save();
+});
+
+function swap() {
+  if (document.getElementById("swap").checked == true) {
     theme.href = "newStyle.css";
   } else {
     theme.href = "style.css";
   }
-});
+}
+
+function save() {
+  localStorage.setItem("swap", swich.checked);
+}
 
 playButton.addEventListener("click", () => {
-  playGame()
-  numTryFocus = document.querySelector('#numTry')
-  numTryFocus.focus()
+  playGame();
+  numTryFocus = document.querySelector('#numTry');
+  numTryFocus.focus();
 });
 
 reSetButton.addEventListener("click", () => {
-  reSet()
-  minFocus = document.querySelector('#min')
-  minFocus.focus()
+  reSet();
+  minFocus = document.querySelector('#min');
+  minFocus.focus();
 });
